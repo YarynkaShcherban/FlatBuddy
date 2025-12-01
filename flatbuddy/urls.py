53b60edc7efe0b8c094fbeed7ecad5ca1493cc44
Yaryna Panychevska <pa.yaryna@gmail.com>
2025-12-01 16:53:54 +0200

    Changed urls.py in flatbuddy

--- flatbuddy/urls.py
+++ flatbuddy/urls.py
@@ -14,4 +14,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('blockchain/', include('blockchain.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
