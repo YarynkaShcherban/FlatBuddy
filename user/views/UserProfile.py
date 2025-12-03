6520f1e32c0e89188381940f91655638a32c6e04
Yaryna Panychevska <pa.yaryna@gmail.com>
2025-12-03 20:58:09 +0200

    Changed UserProfileView.py

--- user/views/UserProfile.py
+++ user/views/UserProfile.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from blockchain.blockchain import blockchain
+from blockchain.blockchain_db import blockchain
 from ..models import UserProfile, UserPhoto
 from ..serializers import UserProfileSerializer
 from .BaseView import BaseViewSet
@@ -11,19 +11,17 @@ class UserProfileViewSet(BaseViewSet):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
 
-    # перевизначила методи, щоб можна було обробляти список файлів
     def create(self, request, *args, **kwargs):
         photos = request.FILES.getlist('photos')
 
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-
-        # створиться профіль + запис у блокчейн
-        profile = self.perform_create(serializer)
+        profile = serializer.save()
 
         for photo in photos:
             UserPhoto.objects.create(user_profile=profile, image=photo)
 
+        profile = self.perform_create(serializer)
         return Response(UserProfileSerializer(profile).data)
 
     def update(self, request, *args, **kwargs):
@@ -34,10 +32,10 @@ class UserProfileViewSet(BaseViewSet):
         serializer = self.get_serializer(
             instance, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
-
-        profile = self.perform_update(serializer)
+        profile = serializer.save()
 
         for photo in photos:
-            UserPhoto.objects.update(user_profile=profile, image=photo)
+            UserPhoto.objects.create(user_profile=profile, image=photo)
 
+        profile = self.perform_update(serializer)
         return Response(UserProfileSerializer(profile).data)
--- user/views/UserView.py
+++ user/views/UserView.py
@@ -1,10 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from blockchain.blockchain import blockchain
+from blockchain.blockchain_db import blockchain
 from ..models import User
 from ..serializers import UserSerializer
 from .BaseView import BaseViewSet
+
 
 class UserViewSet(BaseViewSet):
     queryset = User.objects.all()
